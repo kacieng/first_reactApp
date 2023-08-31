9c359ee77f0febbe4a2188d3a594654fd6ceb782
Kacie Ng <kacie.ng@Kacies-MacBook-Pro.local>
2023-08-31 11:10:40 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, this is a React app</h1>
+      <button type="button">Click Me!</button>
+    </div>
+  );
+}
+
+export default App;
